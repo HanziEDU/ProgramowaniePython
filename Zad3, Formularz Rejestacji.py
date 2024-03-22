aaed8e61ec6c4145d76a3de82e8b9ecc7c3855f2
Hanzi <miki.handzlikpl@gmail.com>
2024-03-22 01:17:44 +0100

    Zadania

--- Zad3, Formularz Rejestacji.py
+++ Zad3, Formularz Rejestacji.py
@@ -1,92 +1,70 @@
 def is_letter(char):
     return char.isalpha()
 
-
 def is_digit(char):
     return char.isdigit()
 
-
 def validate_date(date_str):
     day, month, year = date_str.split('-')
-
     if len(day) != 2 or len(month) != 2 or len(year) != 4:
         return False
-
     if not (day.isdigit() and month.isdigit() and year.isdigit()):
         return False
-
     if int(month) < 1 or int(month) > 12:
         return False
-
     if int(day) < 1 or int(day) > 31:
         return False
-
     return True
-
 
 def validate_email(email):
-    parts = email.split('@')
-
-    if len(parts) != 2:
+    if email.count('@') != 1:
         return False
-
-    local_part, domain_part = parts
-    if not is_letter(local_part[0]):
+    if email[0].isdigit():
         return False
-
-    if '.' not in domain_part or not domain_part.split('.')[0].isalpha():
+    if '.' not in email.split('@')[1]:
         return False
-
     return True
 
-
 def encrypt_name(name):
+    parts = name.split()
     encrypted_name = ''
-    for char in name:
-        if is_letter(char):
-            encrypted_name += char.upper()
-        else:
-            encrypted_name += char
-    return encrypted_name
-
+    for part in parts:
+        encrypted_part = part[0].upper() + '*' * (len(part) - 1)
+        encrypted_name += encrypted_part + ' '
+    return encrypted_name.strip()
 
 def encrypt_email(email):
     local_part, domain_part = email.split('@')
-    encrypted_local = local_part[0].upper() + '*' * (len(local_part) - 1)
-    return encrypted_local + '@' + domain_part
+    encrypted_local_part = local_part[0].upper() + '*' * (len(local_part) - 1)
+    return encrypted_local_part + '@' + domain_part
 
-
-def calculate_age(birth_year):
+def calculate_age(year_of_birth):
     current_year = 2024
-    return current_year - int(birth_year)
+    return current_year - year_of_birth
+
+name = input("Podaj imię: ")
+surname = input("Podaj nazwisko: ")
+date_of_birth = input("Podaj datę urodzenia(dd-mm-rrrr): ")
+email = input("Podaj adres e-mail: ")
 
 
-def main():
-    name = input("Podaj imię i nazwisko: ")
-    birth_date = input("Podaj datę urodzenia w formacie dd-mm-rrrr: ")
-    email = input("Podaj adres e-mail: ")
+if not validate_email(email):
+    print("Błędny format adresu e-mail!")
+    exit()
 
-    if not validate_date(birth_date):
-        print("Nieprawidłowy format daty urodzenia.")
-        return
+if not validate_date(date_of_birth):
+    print("Błędny format daty urodzenia!")
+    exit()
 
-    if not validate_email(email):
-        print("Nieprawidłowy format adresu e-mail.")
-        return
-
-    encrypted_name = encrypt_name(name)
-    encrypted_email = encrypt_email(email)
-    age = calculate_age(birth_date.split('-')[-1])
-
-    user_data = {
-        'imię i nazwisko': encrypted_name,
-        'wiek': age,
-        'mail': encrypted_email
-    }
-
-    print("Wprowadzone dane:")
-    print(user_data)
+encrypted_name = encrypt_name(name + ' ' + surname)
+age = calculate_age(int(date_of_birth.split('-')[2]))
+encrypted_email = encrypt_email(email)
 
 
-if __name__ == "__main__":
-    main()
+user_data = {
+    "Imię i Nazwisko": encrypted_name,
+    "Wiek": age,
+    "Mail": encrypted_email
+}
+
+print(user_data)
